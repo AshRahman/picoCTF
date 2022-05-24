b158d67763524bdfa6966271cd56ba48c639ac53
AshRahman <ashfaqurrahmanrahat@outlook.com>
2022-05-23 20:55:54 -0700

    yesterdays commit

---
+++ PW Crack 5/level5.py
@@ -0,0 +1,48 @@
+import hashlib
+
+### THIS FUNCTION WILL NOT HELP YOU FIND THE FLAG --LT ########################
+def str_xor(secret, key):
+    #extend key to secret length
+    new_key = key
+    i = 0
+    while len(new_key) < len(secret):
+        new_key = new_key + key[i]
+        i = (i + 1) % len(key)        
+    return "".join([chr(ord(secret_c) ^ ord(new_key_c)) for (secret_c,new_key_c) in zip(secret,new_key)])
+###############################################################################
+
+flag_enc = open('level5.flag.txt.enc', 'rb').read()
+correct_pw_hash = open('level5.hash.bin', 'rb').read()
+#dictionary=open('dictionary.txt','r')
+#dictionary=dictionary.strip()
+
+#with open('dictionary.txt') as f:
+ #   mylist = list(f)
+
+with open('dictionary.txt') as f:
+    mylist = [line.rstrip('\n') for line in f]
+
+def hash_pw(pw_str):
+    pw_bytes = bytearray()
+    pw_bytes.extend(pw_str.encode())
+    m = hashlib.md5()
+    m.update(pw_bytes)
+    return m.digest()
+
+
+def level_5_pw_check():
+	for x in mylist:
+		user_pw = x
+		user_pw_hash = hash_pw(user_pw)
+		print(user_pw)
+		if( user_pw_hash == correct_pw_hash ):
+			print("Welcome back... your flag, user:")
+			decryption = str_xor(flag_enc.decode(), user_pw)
+			print(decryption)
+			return
+			print("That password is incorrect")
+
+
+
+level_5_pw_check()
+
---
+++ PW Crack 5/trim.py
@@ -0,0 +1,5 @@
+dictionary=open('dictionary.txt','r')
+#dictionary=dictionary.rstrip()
+print(type(dictionary))
+for x in dictionary:
+	print(x)
